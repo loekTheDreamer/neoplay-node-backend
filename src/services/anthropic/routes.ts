8c81444e925e8d8f4f169739e8d0a2c895fdf600
Luke Carelsen <luke.carelsen@me.com>
2025-03-31 13:25:40 +0200

    fix: claude

--- src/services/anthropic/routes.ts
+++ src/services/anthropic/routes.ts
@@ -14,9 +14,9 @@ interface ChatRequest {
   systemPrompt?: string;
 }
 
-// const model = 'claude-3-7-sonnet-20250219';
+const model = 'claude-3-7-sonnet-20250219';
 // const model2 = "claude-3-5-sonnet-20241022"
-const model = 'claude-3-5-haiku-20241022';
+// const model = 'claude-3-5-haiku-20241022';
 
 export function registerAnthropicRoutes(fastify: FastifyInstance) {
   // Root Route
@@ -26,18 +26,18 @@ export function registerAnthropicRoutes(fastify: FastifyInstance) {
 
   fastify.post('/chat', async (request, reply) => {
     const { chatHistory, systemPrompt } = request.body as ChatRequest;
-    // console.log('chatHistory:', chatHistory);
-    // console.log('prompt:', systemPrompt);
+    console.log('chatHistory:', chatHistory);
+    console.log('prompt:', systemPrompt);
 
     const response = await client.messages.create({
-      // max_tokens: 20000, // claude-3-7-sonnet
-      max_tokens: 8192, // claude-3-5-haiku
+      max_tokens: 20000, // claude-3-7-sonnet
+      // max_tokens: 8192, // claude-3-5-haiku
       messages: chatHistory,
-      model: model
-      // system: systemPrompt
+      model: model,
+      system: systemPrompt
     });
 
-    console.log('response:', response);
+    // console.log('response:', response);
 
     return response;
   });
