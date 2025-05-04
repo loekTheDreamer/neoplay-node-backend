13402e8dcf308ddc73e9751d57bdd674b74d1974
Luke Carelsen <luke.carelsen@me.com>
2025-05-04 13:03:29 +0200

    test: output

--- src/services/db/prisma.ts
+++ src/services/db/prisma.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+console.log('DATABASE_URL from process.env:', process.env.DATABASE_URL);
+console.log('NODE_ENV:', process.env.NODE_ENV);
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
