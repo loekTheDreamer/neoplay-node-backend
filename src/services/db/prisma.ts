a81c5cd653455092eb6fd99a930f91427f5ea35d
Luke Carelsen <luke.carelsen@me.com>
2025-05-04 13:07:18 +0200

    test: another

--- src/services/db/prisma.ts
+++ src/services/db/prisma.ts
@@ -6,10 +6,14 @@ dotenv.config();
 console.log('DATABASE_URL from process.env:', process.env.DATABASE_URL);
 console.log('NODE_ENV:', process.env.NODE_ENV);
 
+// Force the URL to start with postgresql://
+const databaseUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL.replace(/^postgres:\/\/$/, 'postgresql://') : process.env.DATABASE_URL;
+console.log('Modified DATABASE_URL for Prisma:', databaseUrl);
+
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: databaseUrl,
     },
   },
 });
